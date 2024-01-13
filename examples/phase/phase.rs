5f3fa654b0fbf7fdfc98e5f256a5110bd0ed0f37
Per Lindgren <per.lindgren@ltu.se>
2024-01-13 01:09:51 +0100

    lib

--- examples/phase/phase.rs
+++ examples/phase/phase.rs
@@ -1,6 +1,5 @@
 // phase
 
-use plotters::prelude::*;
 use std::f64::consts::PI;
 
 fn main() {
@@ -29,66 +28,7 @@ fn main() {
         unwrapped.push(wrap * 2.0 * PI + new_atan2);
     }
 
-    // plot left, right
-    let root_area =
-        SVGBackend::new("examples/phase/left_right.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("left right", ("sans-serif", 40))
-        .build_cartesian_2d(0..SF, -1.0..1.0)
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, left[x])), &BLUE))
-        .unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, right[x])), &RED))
-        .unwrap();
-
-    // plot atan2
-    let root_area = SVGBackend::new("examples/phase/atan2.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("atan2", ("sans-serif", 40))
-        .build_cartesian_2d(0..SF, -PI..PI)
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, wrapped[x])), &RED))
-        .unwrap();
-
-    // plot unwrapped
-    let root_area = SVGBackend::new("examples/phase/unwrapped.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("unwrapped", ("sans-serif", 40))
-        .build_cartesian_2d(
-            0..SF,
-            -PI..(unwrapped
-                .clone()
-                .into_iter()
-                .fold(f64::NEG_INFINITY, f64::max)),
-        )
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, unwrapped[x])), &RED))
-        .unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, wrapped[x])), &BLUE))
-        .unwrap();
+    phase::plot_all(left, right, wrapped, unwrapped.clone(), SF, "phase");
 
     phase::quality(unwrapped, "octave/phase.csv");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 // lib
-
+use plotters::prelude::*;
 use std::error::Error;
+use std::f64::consts::PI;
 
-pub fn read_file(path: &str) -> Result<Vec<f64>, Box<dyn Error>> {
+fn read_file(path: &str) -> Result<Vec<f64>, Box<dyn Error>> {
     let mut rdr = csv::ReaderBuilder::new()
         .has_headers(false)
         .from_path(path)
@@ -28,4 +29,87 @@ pub fn quality(rust: Vec<f64>, path: &str) {
 
     println!("accumulated error {}", acc);
     println!("min square error  {}", mse / rust.len() as f64);
+}
+
+// plot left, right
+pub fn plot_left_right(left: Vec<f64>, right: Vec<f64>, sf: usize, path: &str) {
+    let path = &format!("examples/{}/left_right.svg", path);
+    let root_area = SVGBackend::new(path, (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("left right", ("sans-serif", 40))
+        .build_cartesian_2d(0..sf, -1.0..1.0)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(LineSeries::new((0..sf).map(|x| (x, left[x])), &BLUE))
+        .unwrap();
+
+    ctx.draw_series(LineSeries::new((0..sf).map(|x| (x, right[x])), &RED))
+        .unwrap();
+}
+
+// plot wrapped, atan2
+pub fn plot_wrapped(wrapped: Vec<f64>, sf: usize, path: &str) {
+    let path = &format!("examples/{}/wrapped.svg", path);
+    let root_area = SVGBackend::new(path, (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("wrapped, atan2", ("sans-serif", 40))
+        .build_cartesian_2d(0..sf, -PI..PI)
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(LineSeries::new((0..sf).map(|x| (x, wrapped[x])), &RED))
+        .unwrap();
+}
+
+// plot unwrapped
+pub fn plot_unwrapped(unwrapped: Vec<f64>, wrapped: Vec<f64>, sf: usize, path: &str) {
+    let path = &format!("examples/{}/unwrapped.svg", path);
+    let root_area = SVGBackend::new(path, (600, 400)).into_drawing_area();
+    root_area.fill(&WHITE).unwrap();
+
+    let mut ctx = ChartBuilder::on(&root_area)
+        .set_label_area_size(LabelAreaPosition::Left, 40)
+        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .caption("unwrapped", ("sans-serif", 40))
+        .build_cartesian_2d(
+            0..sf,
+            -PI..(unwrapped
+                .clone()
+                .into_iter()
+                .fold(f64::NEG_INFINITY, f64::max)),
+        )
+        .unwrap();
+
+    ctx.configure_mesh().draw().unwrap();
+
+    ctx.draw_series(LineSeries::new((0..sf).map(|x| (x, unwrapped[x])), &RED))
+        .unwrap();
+
+    ctx.draw_series(LineSeries::new((0..sf).map(|x| (x, wrapped[x])), &BLUE))
+        .unwrap();
+}
+
+// plot all
+pub fn plot_all(
+    left: Vec<f64>,
+    right: Vec<f64>,
+    wrapped: Vec<f64>,
+    unwrapped: Vec<f64>,
+    sf: usize,
+    path: &str,
+) {
+    plot_left_right(left, right, sf, path);
+    plot_wrapped(wrapped.clone(), sf, path);
+    plot_unwrapped(unwrapped, wrapped, sf, path);
 }
