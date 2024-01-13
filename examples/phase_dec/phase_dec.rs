b191a9a8fc639b45a4fc5a9ed85341b513d6be7f
Per Lindgren <per.lindgren@ltu.se>
2024-01-13 01:14:35 +0100

    phase_dec

--- examples/phase_dec/phase_dec.rs
+++ examples/phase_dec/phase_dec.rs
@@ -1,6 +1,5 @@
-// phase
+// phase_dec
 
-use plotters::prelude::*;
 use std::f64::consts::PI;
 
 fn main() {
@@ -34,67 +33,7 @@ fn main() {
         unwrapped.push(wrap * 2.0 * PI + new_atan2);
     }
 
-    // plot left, right
-    let root_area =
-        SVGBackend::new("examples/phase_dec/left_right.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("left right", ("sans-serif", 40))
-        .build_cartesian_2d(0..SF, -1.0..1.0)
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, left[x])), &BLUE))
-        .unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, right[x])), &RED))
-        .unwrap();
-
-    // plot atan2
-    let root_area = SVGBackend::new("examples/phase_dec/atan2.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("atan2", ("sans-serif", 40))
-        .build_cartesian_2d(0..SF, -PI..PI)
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, wrapped[x])), &RED))
-        .unwrap();
-
-    // plot unwrapped
-    let root_area =
-        SVGBackend::new("examples/phase_dec/unwrapped.svg", (600, 400)).into_drawing_area();
-    root_area.fill(&WHITE).unwrap();
-
-    let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .caption("unwrapped", ("sans-serif", 40))
-        .build_cartesian_2d(
-            0..SF,
-            -PI..(unwrapped
-                .clone()
-                .into_iter()
-                .fold(f64::NEG_INFINITY, f64::max)),
-        )
-        .unwrap();
-
-    ctx.configure_mesh().draw().unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, unwrapped[x])), &RED))
-        .unwrap();
-
-    ctx.draw_series(LineSeries::new((0..SF).map(|x| (x, wrapped[x])), &BLUE))
-        .unwrap();
+    phase::plot_all(left, right, wrapped, unwrapped.clone(), SF, "phase_dec");
 
     phase::quality(unwrapped, "octave/phase_dec.csv");
 }
